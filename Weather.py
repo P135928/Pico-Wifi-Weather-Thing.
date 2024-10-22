676dc66ddcffbc45c341e26dae86227ae0c23533
Zackary Zielinski <161618228+P135928@users.noreply.github.com>
2024-10-22 08:20:44 -0700

    Update Weather.py
    Put some extra comments and some organization things in place.

--- Weather.py
+++ Weather.py
@@ -15,16 +15,16 @@ import network
 import gc
 gc.collect()
 
-ssid = ''
-password = ''
+ssid = ' ' # Put your SSID here
+password = 'password' # Put your WiFi Password here
 
-city = 'Phoenix'
-country_code = 'US'
+city = 'Phoenix' # This is the city you live in
+country_code = 'US' # This is your 2 letter country code, I think it can be more too.
 #example
 #city = 'Lahore'
 #country_code = 'PAK'
 
-open_weather_map_api_key = ''
+open_weather_map_api_key = '' # Your API Key, make sure to put this there! Otherwise the program wont be able to communicate to OWM.
 
 station = network.WLAN(network.STA_IF)
 
@@ -41,33 +41,34 @@ print(station.ifconfig())
 open_weather_map_url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + ',' + country_code + '&APPID=' + open_weather_map_api_key
 
 weather_data = requests.get(open_weather_map_url)
-print(weather_data.json())
 
 # Location (City and Country code)
 location = 'Location: ' + weather_data.json().get('name') + ' - ' + weather_data.json().get('sys').get('country')
-print(location)
 
 # Weather Description
 description = 'Description: ' + weather_data.json().get('weather')[0].get('main')
-print(description)
 
 # Temperature
 raw_temperature = weather_data.json().get('main').get('temp')-273.15
 
 # Temperature in Celsius
-temperature = 'Temperature: ' + str(raw_temperature) + '*C'
+#temperature = 'Temperature: ' + str(raw_temperature) + '*C'
 #uncomment for temperature in Fahrenheit
-#temperature = 'Temperature: ' + str(raw_temperature*(9/5.0)+32) + '*F'
-print(temperature)
+temperature = 'Temperature: ' + str(raw_temperature*(9/5.0)+32) + '*F'
 
 # Pressure
 pressure = 'Pressure: ' + str(weather_data.json().get('main').get('pressure')) + 'hPa'
-print(pressure)
 
 # Humidity
 humidity = 'Humidity: ' + str(weather_data.json().get('main').get('humidity')) + '%'
-print(humidity)
 
 # Wind
 wind = 'Wind: ' + str(weather_data.json().get('wind').get('speed')) + 'mps ' + str(weather_data.json().get('wind').get('deg')) + '*'
+
+print("###################################################")
 print(wind)
+print(pressure)
+print(temperature)
+print(description)
+print(location)
+print("###################################################")
